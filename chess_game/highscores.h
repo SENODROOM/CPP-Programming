690df1cc733bdb0951afbb5ead1623bc531b4987
Muhammad Saad Amin <saadamin691@gmail.com>
2026-01-22 16:40:35 +0500

    Chess

--- chess_game/highscores.h
+++
@@ -1,8 +0,0 @@
-#ifndef HIGHSCORES_H
-#define HIGHSCORES_H
-
-void loadHighscores(const char* filename);
-void saveHighscores(const char* filename);
-void recordWin(char winner); // 'w' or 'b'
-
-#endif // HIGHSCORES_H
--- chess_game/save.h
+++
@@ -1,7 +0,0 @@
-#ifndef SAVE_H
-#define SAVE_H
-
-void saveGameToFile(const char* filename);
-bool loadGameFromFile(const char* filename);
-
-#endif // SAVE_H
